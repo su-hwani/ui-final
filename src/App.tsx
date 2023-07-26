4d2135753517023317667da36699901805e2c39c
Pilmo <pilmokim99@gmail.com>
2023-07-26 17:18:42 +0900

    init projects

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import "./App.css";
+import Header from "./components/Header";
+import ChatList from "./components/ChatList";
+import Chat from "./components/Chat";
+import Graph from "./components/Graph";
+
+function App() {
+  return (
+    <>
+      <Header />
+      <ChatList />
+      <Chat />
+      <Graph />
+    </>
+  );
+}
+
+export default App;
