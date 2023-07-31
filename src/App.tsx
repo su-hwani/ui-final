e672593e2e782692f612bd55982499027c85ecf1
wjdtnghks123 <wjdtnghks123@nate.com>
2023-07-31 19:31:50 +0900

    Style: Delete Chat

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@
 
 import React from 'react';
 import ChatList from './components/ChatList';
+import Chat from './components/Chat';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <ChatList />
+      <Chat chatName="Chat 1" />
     </div>
   );
 }
