8e21cc5189a6494fbaf05ab4f552a77640f58de0
wjdtnghks123 <wjdtnghks123@nate.com>
2023-08-03 16:12:51 +0900

    Fix: Change icon location

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -24,13 +24,13 @@ const ChatList: React.FC = () => {
   return (
     <div className=" flex h-screen border-obzen-purple border-solid ">
       <div className=" overflow-auto w-1/4 py-4 px-4 border-r  border-obzen-purple border-solid">
-        <button className="mb-4 p-1 h-12 w-12 font-bold  align-middle text-obzen-purple border-solid border-2 border-obzen-purple  bg-white rounded-lg " onClick={handleAddChat}>
+        <button className="mb-4 p-1 h-12 w-12 font-bold  text-obzen-purple border-solid border-2 border-obzen-purple  bg-white rounded-lg " onClick={handleAddChat}>
           <TbMessageCirclePlus  className="h-8 w-8 "/>
         </button>
         {chats.map((chatName) => (
-          <div key={chatName} className={` p-1  hover:bg-violet-50 cursor-pointer  text-obzen-purple ${selectedChat === chatName ? 'bg-violet-100 rounded-lg' : 'bg-white rounded-lg'}`} onClick={() => handleChatClick(chatName)}>
+          <div key={chatName} className={` p-1 h-14 hover:bg-violet-50 cursor-pointer  text-obzen-purple ${selectedChat === chatName ? 'bg-violet-100 rounded-lg' : 'bg-white rounded-lg'}`} onClick={() => handleChatClick(chatName)}>
             <text className="border-r font-bold align-middle text-lg ">{chatName}</text>
-            <button className="ml-40 p-1 rounded-lg border-solid border-2 border-obzen-purple" onClick={() => handleRemoveChat(chatName)}>
+            <button className=" float-right p-1 rounded-lg border-solid border-2 border-obzen-purple" onClick={() => handleRemoveChat(chatName)}>
               <TbMessageCircleX className="h-8 w-8"/>
             </button>
           </div>
