e61daa7127551c056c8edaa67c9be01f147da4f3
wjdtnghks123 <wjdtnghks123@nate.com>
2023-08-03 15:49:09 +0900

    Feat: #7 Change icon & Chatlist design

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,7 +1,14 @@
 // ChatList.tsx
 
+<<<<<<< Updated upstream
 import React, { useState } from 'react';
 import Chat from './Chat';
+=======
+import React, { useState } from "react";
+import Chat from "./Chat";
+import { BiSolidMessageRoundedAdd } from "react-icons/bi";
+import { TbMessageCirclePlus,TbMessageCircleX } from "react-icons/tb";
+>>>>>>> Stashed changes
 
 const ChatList: React.FC = () => {
   const [chats, setChats] = useState<string[]>(['1번 채팅창']);
@@ -20,8 +27,8 @@ const ChatList: React.FC = () => {
     setChats((prevChats) => prevChats.filter((chat) => chat !== chatName));
     setSelectedChat(chatName);
   };
-
   return (
+<<<<<<< Updated upstream
     <div className="chat-list">
       <div className="chat-sidebar">
         
@@ -35,6 +42,23 @@ const ChatList: React.FC = () => {
       </div>
       {/* 윗부분 */}
       <div className="chat-content">
+=======
+    <div className="flex h-screen border-obzen-purple border-solid ">
+      <div className="w-1/4 py-4 px-2 border-r  border-obzen-purple border-solid">
+        <button className="mb-4 p-1 h-12 w-12 font-bold  align-middle text-obzen-purple border-solid border-2 border-obzen-purple  bg-white rounded-lg " onClick={handleAddChat}>
+          <TbMessageCirclePlus  className="h-8 w-8 "/>
+        </button>
+        {chats.map((chatName) => (
+          <div key={chatName} className={`p-2 hover:bg-violet-50 cursor-pointer  text-obzen-purple ${selectedChat === chatName ? 'bg-violet-100 rounded-lg' : 'bg-white rounded-lg'}`} onClick={() => handleChatClick(chatName)}>
+            <text className="border-r font-bold align-middle text-lg ">{chatName}</text>
+            <button className="ml-48 p-1 rounded-lg border-solid border-2 border-obzen-purple" onClick={() => handleRemoveChat(chatName)}>
+              <TbMessageCircleX className="h-8 w-8"/>
+            </button>
+          </div>
+        ))}
+      </div>
+      <div className="flex-1 p-4 ">
+>>>>>>> Stashed changes
         {selectedChat && <Chat chatName={selectedChat} />}
       </div>
       {/* 윗부분 */}
