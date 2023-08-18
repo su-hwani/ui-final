d6e49e410d7bacd89d7e0590d0b888a7ae003ecb
YunHaaaa <qlxqlrt2012@gmail.com>
2023-08-18 21:53:18 +0900

    Feat: Change chat list design

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,19 +1,17 @@
-// ChatList.tsx
-
 import React, { useState } from "react";
 import Chat from "./Chat";
-import { TbMessageCirclePlus,TbMessageCircleX } from "react-icons/tb";
+import { FaPlus, FaTrash } from "react-icons/fa";
 
 const ChatList: React.FC = () => {
-  const [chats, setChats] = useState<string[]>(['1번 채팅창']);
-  const [selectedChat, setSelectedChat] = useState<string>('1번 채팅창');
+  const [chats, setChats] = useState<string[]>(["1번 채팅창"]);
+  const [selectedChat, setSelectedChat] = useState<string>("1번 채팅창");
 
   const handleChatClick = (chatName: string) => {
     setSelectedChat(chatName);
   };
 
   const handleAddChat = () => {
-    const newChatName = `${chats.length + 1}번 채팅창`; //채팅 제목 넣는 곳 -> 따로 받으면 될 듯
+    const newChatName = `${chats.length + 1}번 채팅창`;
     setChats((prevChats) => [...prevChats, newChatName]);
   };
 
@@ -21,17 +19,33 @@ const ChatList: React.FC = () => {
     setChats((prevChats) => prevChats.filter((chat) => chat !== chatName));
     setSelectedChat(chatName);
   };
+
   return (
-    <div className="flex h-screen border-obzen-purple border-solid">
-      <div className="overflow-auto w-1/4 py-4 px-4 border-r border-default-border border-solid">
-        <button className="mb-4 p-1 h-12 w-12 font-bold text-obzen-purple border-solid border-2 border-obzen-purple bg-white rounded-lg" onClick={handleAddChat}>
-          <TbMessageCirclePlus className="h-8 w-8" />
+    <div className="flex h-screen">
+      <div className="w-1/4 py-4 px-4 border-r border-charcoal border-solid">
+        <button
+          className="mb-4 p-2 flex items-center justify-center font-bold text-charcoal border-solid border-2 border-charcoal bg-white rounded-lg w-full"
+          onClick={handleAddChat}
+        >
+          <FaPlus className="h-5 w-5 mr-2" />
+          채팅 추가
         </button>
         {chats.map((chatName) => (
-          <div key={chatName} className={`p-1 h-14 hover:bg-violet-100 cursor-pointer ${selectedChat === chatName ? 'bg-violet-100 rounded-lg' : 'bg-white rounded-lg'} border-r`} onClick={() => handleChatClick(chatName)}>
-            <text className="font-bold align-middle text-lg text-obzen-purple">{chatName}</text>
-            <button className="float-right p-1 rounded-lg border-solid border-2 border-obzen-purple" onClick={() => handleRemoveChat(chatName)}>
-              <TbMessageCircleX className="h-8 w-8 text-obzen-purple" />
+          <div
+            key={chatName}
+            className={`p-2 flex items-center justify-between hover:bg-gray-100 cursor-pointer ${
+              selectedChat === chatName
+                ? "bg-gray-300 rounded-lg"
+                : "rounded-lg"
+            } border-r`}
+            onClick={() => handleChatClick(chatName)}
+          >
+            <span className="font-bold text-lg text-gray">{chatName}</span>
+            <button
+              className="p-1 rounded-lg"
+              onClick={() => handleRemoveChat(chatName)}
+            >
+              <FaTrash className="h-4 w-4 text-gray" />
             </button>
           </div>
         ))}
@@ -39,7 +53,6 @@ const ChatList: React.FC = () => {
       <div className="flex-1 p-4">
         {selectedChat && <Chat chatName={selectedChat} />}
       </div>
-      {/* 윗부분 */}
     </div>
   );
 };
