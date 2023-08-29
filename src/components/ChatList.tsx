ecdd0e52e478b9d5774f0a465bb0d2e63cfca741
YunHaaaa <qlxqlrt2012@gmail.com>
2023-08-29 10:50:05 +0900

    Fix: Resolving Merge Errors

--- src/components/ChatList.tsx
+++ src/components/ChatList.tsx
@@ -1,8 +1,7 @@
-import React, { useState } from "react";
-import Chat from "./Chat";
-import { FaPlus, FaTrash, FaComment } from "react-icons/fa"; // Import the FaComment icon
+import { FaPlus, FaTrash, FaComment } from "react-icons/fa";
+import { useState } from "react";
 
-const ChatList: React.FC = () => {
+export default function ChatList() {
   const [chats, setChats] = useState<string[]>(["1번 채팅창"]);
   const [selectedChat, setSelectedChat] = useState<string>("1번 채팅창");
 
@@ -13,54 +12,52 @@ const ChatList: React.FC = () => {
   const handleAddChat = () => {
     const newChatName = `${chats.length + 1}번 채팅창`;
     setChats((prevChats) => [...prevChats, newChatName]);
+    setSelectedChat(newChatName); // 새로 추가된 채팅으로 자동 선택
   };
 
   const handleRemoveChat = (chatName: string) => {
-    setChats((prevChats) => prevChats.filter((chat) => chat !== chatName));
-    setSelectedChat(chatName);
+    const updatedChats = chats.filter((chat) => chat !== chatName);
+    setChats(updatedChats);
+
+    // 삭제한 채팅이 선택되어 있었다면 선택 해제
+    if (selectedChat === chatName) {
+      setSelectedChat("");
+    }
   };
 
   return (
-    <div className="flex h-screen">
-      <div className="w-1/4 py-4 px-4 border-r border-charcoal border-solid">
-        <button
-          className="mb-4 p-2 flex items-center justify-center font-bold text-charcoal border-solid border-2 border-charcoal bg-white rounded-lg w-full"
-          onClick={handleAddChat}
+    <div className="overflow-auto w-1/4 py-4 px-4 border-r border-default-border bg-list-background">
+      <button
+        className="mb-4 p-2 h-12 w-16 font-bold text-white border-solid border border-0.2 border-white bg-list-background rounded-lg"
+        onClick={handleAddChat}
+      >
+        <FaPlus className="h-6 w-6" />
+        <span className="text-white">New chat</span>
+      </button>
+      {chats.map((chatName) => (
+        <div
+          key={chatName}
+          className={`p-2 h-14 flex items-center justify-between hover:bg-list-selected cursor-pointer ${
+            selectedChat === chatName
+              ? "bg-list-selected rounded-lg"
+              : "bg-list-background rounded-lg"
+          } border-r`}
+          onClick={() => handleChatClick(chatName)}
         >
-          <FaPlus className="h-4 w-5 mr-2" />
-          New chat
-        </button>
-        {chats.map((chatName) => (
-          <div
-            key={chatName}
-            className={`p-2 flex items-center justify-between hover:bg-gray-100 cursor-pointer ${
-              selectedChat === chatName
-                ? "bg-gray-300 rounded-lg"
-                : "rounded-lg"
-            } border-r`}
-            onClick={() => handleChatClick(chatName)}
-          >
-            <span className="flex items-center space-x-2">
-            <FaComment className="h-5 w-5 text-gray pointer-events-none" />
-              <span className="font-medium text-lg text-gray">{chatName}</span>
+          <div className="flex items-center space-x-2">
+            <FaComment className="h-5 w-5 text-white" />
+            <span className="font-bold text-lg text-white">
+              {chatName}
             </span>
-            <button
-              className="p-1 rounded-lg hover:text-gray-500"
-              onClick={() => handleRemoveChat(chatName)}
-            >
-              <span className="text-xl">
-                <FaTrash className="h-4 w-4 text-gray" />
-              </span>
-            </button>
           </div>
-        ))}
-      </div>
-      <div className="flex-1 p-4">
-        {selectedChat && <Chat chatName={selectedChat} />}
-      </div>
+          <button
+            className={`p-2 rounded-full bg-list-background`}
+            onClick={() => handleRemoveChat(chatName)}
+          >
+            <FaTrash className="h-4 w-4 text-white" />
+          </button>
+        </div>
+      ))}
     </div>
   );
-};
-
-
-export default ChatList;
+}
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,10 +3,15 @@ module.exports = {
   content: ["./src/**/*.{ts,tsx}"],
   theme: {
     extend: {
+      borderWidth: {
+        '1.5': '1.5px',
+      },
       colors: {
         "background-blue": "#f0f4f8",
         "detail-black": "#6b5e71",
         "chart-background": "#fffefe",
+        "list-background" : "#181C1F",
+        "list-selected" : "#2D3438",
 
         "obzen-purple": "#672c68",
         "obzen-red" : "#E04C4C",
