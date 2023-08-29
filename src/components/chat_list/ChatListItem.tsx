2a1a516db379c5a87d2c262d7f19347c90f071cd
YunHaaaa <qlxqlrt2012@gmail.com>
2023-08-29 14:57:54 +0900

    Design: Match design when hover (with delay)

--- src/components/chat_list/ChatListItem.tsx
+++ src/components/chat_list/ChatListItem.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { BiComment, BiTrash } from "react-icons/bi";
 
 interface ChatListItemProps {
@@ -14,31 +14,32 @@ const ChatListItem: React.FC<ChatListItemProps> = ({
   onChatClick,
   onTrashClick,
 }) => {
+  const [isHovered, setIsHovered] = useState(false); // State to track hover
+
   const chatBackgroundClass = isSelected
     ? "bg-list-selected rounded-lg"
     : "bg-list-background rounded-lg";
 
-  const gradientBackgroundClass = isSelected
+  const gradientBackgroundClass = (isSelected || isHovered)
     ? "from-list-selected rounded-lg"
     : "from-list-background rounded-lg";
 
   return (
     <div
-      role="button" // Use role="button" to indicate it's an interactive element
+      role="button"
       className={`flex py-3 px-3 items-center gap-3 relative rounded-md cursor-pointer break-all pr-14 hover:bg-list-selected group ${chatBackgroundClass}`}
       onClick={() => onChatClick(chatName)}
-      style={{ marginBottom: "8px" }} // Add margin to the bottom of each item
+      onMouseEnter={() => setIsHovered(true)} // Set hover state to true
+      onMouseLeave={() => setIsHovered(false)} // Set hover state to false
+      style={{ marginBottom: "8px" }}
     >
-      {/* 말풍선 아이콘 */}
       <BiComment className="h-4 w-4 text-white opacity-50 mr-2" />
-      {/* 채팅 이름 */}
       <div className="flex-1 text-ellipsis max-h-5 overflow-hidden break-all relative text-gray-300">
         {chatName}
         <div
           className={`absolute inset-y-0 right-0 w-8 z-10 bg-gradient-to-l ${gradientBackgroundClass}`}
         ></div>
       </div>
-      {/* 휴지통 아이콘 */}
       <div className="absolute flex right-1 z-10 text-gray-300 visible mr-2">
         <button
           className="p-1 hover:text-white"
