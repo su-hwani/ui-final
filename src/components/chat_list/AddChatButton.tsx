892949ea5c4e5ed825498afa8bb55b63beb51403
Pilmo <pilmokim99@gmail.com>
2023-08-31 01:00:19 +0900

    modify sidbar icons

--- src/components/chat_list/AddChatButton.tsx
+++ src/components/chat_list/AddChatButton.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { BiPlus, BiExpand, BiCollapse } from "react-icons/bi";
-
+import { TbLayoutSidebar, TbLayoutSidebarRight } from "react-icons/tb";
 interface AddChatButtonProps {
   onAddChat: () => void;
   onFullScreenToggle: () => void;
   isFullScreen: boolean;
 }
 
-const AddChatButton: React.FC<AddChatButtonProps> = ({ onAddChat, onFullScreenToggle, isFullScreen }) => {
+const AddChatButton: React.FC<AddChatButtonProps> = ({
+  onAddChat,
+  onFullScreenToggle,
+  isFullScreen,
+}) => {
   return (
     <div className="mb-3 flex flex-col gap-2">
       <div className="flex items-center gap-2">
@@ -24,7 +28,11 @@ const AddChatButton: React.FC<AddChatButtonProps> = ({ onAddChat, onFullScreenTo
           className="flex items-center justify-center gap-2 px-3 min-h-[44px] py-1 text-white text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-11 transition-colors duration-200 cursor-pointer"
         >
           {/* Toggle icon based on isFullScreen state */}
-          {isFullScreen ? <BiCollapse className="h-5 w-5" /> : <BiExpand className="h-5 w-5" />}
+          {isFullScreen ? (
+            <TbLayoutSidebarRight className="h-5 w-5" />
+          ) : (
+            <TbLayoutSidebar className="h-5 w-5" />
+          )}
         </button>
       </div>
     </div>
