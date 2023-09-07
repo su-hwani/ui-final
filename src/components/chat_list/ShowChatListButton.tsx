07cd6c983b8adcad894243bc72a3b026a1659992
YunHaaaa <qlxqlrt2012@gmail.com>
2023-09-07 18:49:11 +0900

    Design: modify show chat list button design

--- src/components/chat_list/ShowChatListButton.tsx
+++ src/components/chat_list/ShowChatListButton.tsx
@@ -21,13 +21,12 @@ const ShowChatListButton: React.FC<ShowChatListButtonProps> = ({
 
   return (
     <div
-      className={`flex items-center justify-center px-3 min-h-[44px] py-1 text-black text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 w-full transition-colors duration-200 cursor-pointer ${
+      className={`flex items-center justify-center pl-5 min-h-[44px] py-10 text-black text-sm rounded-md border border-white/20 hover:bg-gray-500/10 h-11 transition-colors duration-200 cursor-pointer ${
         isChatListVisible ? "hidden" : ""
       }`}
       onClick={handleButtonClick}
     >
       <BiArrowToRight className="h-5 w-5" />
-      <span className="truncate">Show Chat List</span>
     </div>
   );
 };
