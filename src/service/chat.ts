d9eaab519bc2be2979fa9d5a8ae7adc1a5f1b85a
wjdtnghks123 <wjdtnghks123@nate.com>
2023-10-12 23:22:18 +0900

    Feat chat.ts

--- src/service/chat.ts
+++ src/service/chat.ts
@@ -33,4 +33,75 @@ export default class ChatService {
     });
   }
 }
+  // -----------------------------------------------
 
+//   async getSession() {
+//     return this.http.fetch(`/session`, {
+//       method: "GET",
+//       body: JSON.stringify({}), // 메시지를 서버로 전송
+//     });
+//   }
+
+//   async getSessionID() {
+//     return this.http.fetch(`/session/session_id`, {
+//       method: "GET",
+//       body: JSON.stringify({}),
+//     });
+//   }
+
+//   async (chatId: number, text: string) {
+//     return this.http.fetch(`/chats/${chatId}`, {
+//       method: "PUT",
+//       body: JSON.stringify({ text }),
+//     });
+//   }
+//   async updateChat(chatId: number, text: string) {
+//     return this.http.fetch(`/chats/${chatId}`, {
+//       method: "PUT",
+//       body: JSON.stringify({ text }),
+//     });
+//   }
+//   async updateChat(chatId: number, text: string) {
+//     return this.http.fetch(`/chats/${chatId}`, {
+//       method: "PUT",
+//       body: JSON.stringify({ text }),
+//     });
+//   }
+//   async updateChat(chatId: number, text: string) {
+//     return this.http.fetch(`/chats/${chatId}`, {
+//       method: "PUT",
+//       body: JSON.stringify({ text }),
+//     });
+//   }
+//   async updateChat(chatId: number, text: string) {
+//     return this.http.fetch(`/chats/${chatId}`, {
+//       method: "PUT",
+//       body: JSON.stringify({ text }),
+//     });
+//   }
+//   async updateChat(chatId: number, text: string) {
+//     return this.http.fetch(`/chats/${chatId}`, {
+//       method: "PUT",
+//       body: JSON.stringify({ text }),
+//     });
+//   }
+//   async updateChat(chatId: number, text: string) {
+//     return this.http.fetch(`/chats/${chatId}`, {
+//       method: "PUT",
+//       body: JSON.stringify({ text }),
+//     });
+//   }
+//   async updateChat(chatId: number, text: string) {
+//     return this.http.fetch(`/chats/${chatId}`, {
+//       method: "PUT",
+//       body: JSON.stringify({ text }),
+//     });
+//   }
+//   async updateChat(chatId: number, text: string) {
+//     return this.http.fetch(`/chats/${chatId}`, {
+//       method: "PUT",
+//       body: JSON.stringify({ text }),
+//     });
+//   }
+
+// }
